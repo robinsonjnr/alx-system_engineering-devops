38a90d9848b22bcacfd8644c93863432ce1a746e
obinna robinson <obinnarobinson1@gmail.com>
2023-04-23 23:27:52 +0300

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,14 @@
-# SSH client configuration
-exec { 'echo -e "IdentityFile ~/.ssh/holberton\n" >> /etc/ssh/ssh_config':
-  provider => shell,
+# this is to Make passwordAuthentication and add new private key
+file_line { 'replace passwordAuthentication':
+  ensure  => 'present',
   path    => '/etc/ssh/ssh_config',
-  command => 'echo -e "PasswordAuthentication no\n" >> /etc/ssh/ssh_config'
+  replace => true,
+  line    => 'PasswordAuthentication no',
+  match   => 'PasswordAuthentication yes',
+}
+
+file_line { 'add the private key ~/.ssh/school':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
 }
